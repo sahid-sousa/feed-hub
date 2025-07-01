7d8ae4a18663df81e41966bf6c4a12992436a0a1
Sahid Sousa <sahid.sousa@bol.com.br>
2025-06-30 21:51:23 -0300

    first commit

---
+++ src/main/java/br/com/feedhub/FeedHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.feedhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedHubApplication.class, args);
+    }
+
+}
